10f48f0e9a882863a49ca5940cf7c786fedbbb7c
Dan Keder <dan.keder@gmail.com>
2017-08-11 14:29:58 +0200

    Add arguments --left & --right

--- apidiff/__init__.py
+++ apidiff/__init__.py
@@ -14,21 +14,27 @@ class ApiDiffException(Exception):
 
 async def parseargs():
     parser = argparse.ArgumentParser(
-            description='Diff tool for web APIs',
-            add_help=True)
+        description='Diff tool for web APIs',
+        add_help=True)
     parser.add_argument('url_a',
-            type=str,
-            metavar='url_a',
-            help='URL A')
+        type=str,
+        metavar='url_a',
+        help='URL A')
     parser.add_argument('url_b',
-            type=str,
-            metavar='url_b',
-            help='URL B')
+        type=str,
+        metavar='url_b',
+        help='URL B')
     parser.add_argument('-j', '--jq-filter',
-            type=str,
-            metavar='jq_filter',
-            default='.',
-            help='Use jq(1) expression to filter the JSON data before diff')
+        type=str,
+        metavar='jq_filter',
+        default='.',
+        help='Use jq(1) expression to filter the JSON data before diff')
+    parser.add_argument('-l', '--left',
+        action='store_true',
+        help='Print the left-side response only')
+    parser.add_argument('-r', '--right',
+        action='store_true',
+        help='Print the right-side response only')
     return parser.parse_args(sys.argv[1:])
 
 
@@ -76,37 +82,51 @@ async def print_unified_diff(a, b, fromfile='', tofile=''):
 async def run():
     args = await parseargs()
 
-    tasks = [
-        asyncio.ensure_future(request(args.url_a)),
-        asyncio.ensure_future(request(args.url_b)),
-    ]
-
-    resp_a, resp_b = await asyncio.gather(*tasks)
-
-    if resp_a['status'] != resp_b['status']:
-        a = '{} {} {}\n'.format(resp_a['version'], resp_a['status'], resp_a['reason'])
-        b = '{} {} {}\n'.format(resp_b['version'], resp_b['status'], resp_b['reason'])
-        await print_unified_diff(a, b, args.url_a, args.url_b)
-
-    elif resp_a['content_type'] != resp_b['content_type']:
-        a = 'Content-Type: {}\n'.format(resp_a['content_type'])
-        b = 'Content-Type: {}\n'.format(resp_b['content_type'])
-        await print_unified_diff(a, b, args.url_a, args.url_b)
-
-    # Now we can safely assume that the content types of both sides are the same
-    elif 'text' in resp_a:
-        await print_unified_diff(resp_a['text'], resp_b['text'], args.url_a, args.url_b)
-
-    elif 'json' in resp_a:
-        json_a = await jq_filter(resp_a['json'], args.jq_filter)
-        json_b = await jq_filter(resp_b['json'], args.jq_filter)
-        await print_unified_diff(json_a, json_b, args.url_a, args.url_b)
-
-    elif 'bytes' in resp_a:
-        print('Binary data differ')
-
+    tasks = []
+    if args.left:
+        tasks.append(asyncio.ensure_future(request(args.url_a)))
+    elif args.right:
+        tasks.append(asyncio.ensure_future(request(args.url_b)))
     else:
-        ApiDiffException('Error: I dont know what to do. This is probably a bug')
+        tasks.append(asyncio.ensure_future(request(args.url_a)))
+        tasks.append(asyncio.ensure_future(request(args.url_b)))
+
+    if args.left or args.right:
+        resp = (await asyncio.gather(*tasks))[0]
+        print('Status:', resp['status'])
+        print('Content-Type:', resp['content_type'])
+        print()
+        if 'text' in resp:
+            print(resp.get('text'))
+        if 'json' in resp:
+            print(json.dumps(resp['json'], indent=4))
+    else:
+        resp_a, resp_b = await asyncio.gather(*tasks)
+
+        if resp_a['status'] != resp_b['status']:
+            a = '{} {} {}\n'.format(resp_a['version'], resp_a['status'], resp_a['reason'])
+            b = '{} {} {}\n'.format(resp_b['version'], resp_b['status'], resp_b['reason'])
+            await print_unified_diff(a, b, args.url_a, args.url_b)
+
+        elif resp_a['content_type'] != resp_b['content_type']:
+            a = 'Content-Type: {}\n'.format(resp_a['content_type'])
+            b = 'Content-Type: {}\n'.format(resp_b['content_type'])
+            await print_unified_diff(a, b, args.url_a, args.url_b)
+
+        # Now we can safely assume that the content types of both sides are the same
+        elif 'text' in resp_a:
+            await print_unified_diff(resp_a['text'], resp_b['text'], args.url_a, args.url_b)
+
+        elif 'json' in resp_a:
+            json_a = await jq_filter(resp_a['json'], args.jq_filter)
+            json_b = await jq_filter(resp_b['json'], args.jq_filter)
+            await print_unified_diff(json_a, json_b, args.url_a, args.url_b)
+
+        elif 'bytes' in resp_a:
+            print('Binary data differ')
+
+        else:
+            ApiDiffException('Error: I dont know what to do. This is probably a bug')
 
 
 def main():
